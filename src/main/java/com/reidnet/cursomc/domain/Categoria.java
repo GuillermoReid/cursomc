3b25b7e0036aa0436574ad2506a888e3b674247e
Guillermo Reid <g.guireid@gmail.com>
2020-06-29 14:24:14 -0300

    Produto e associacao muito para muito

--- src/main/java/com/reidnet/cursomc/domain/Categoria.java
+++ src/main/java/com/reidnet/cursomc/domain/Categoria.java
@@ -1,21 +1,27 @@
 package com.reidnet.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String nome;
+
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 
 	public Integer getId() {
 		return id;
@@ -31,6 +37,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	public Categoria() {
