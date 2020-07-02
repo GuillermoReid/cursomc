4bf9bdd52e810dd3a431021c740d7ba93daabed5
Guillermo Reid <g.guireid@gmail.com>
2020-07-02 15:49:22 -0300

    PUT DELETE e GET para Cliente

--- src/main/java/com/reidnet/cursomc/services/ClienteService.java
+++ src/main/java/com/reidnet/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.reidnet.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.reidnet.cursomc.domain.Cliente;
+import com.reidnet.cursomc.dto.ClienteDTO;
 import com.reidnet.cursomc.repositories.ClienteRepository;
+import com.reidnet.cursomc.services.exceptions.DataIntegrityException;
 import com.reidnet.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -24,5 +31,41 @@ public class ClienteService {
 		
 	}
 
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	public Cliente update(Cliente obj) {
+		
+		Cliente newObj= find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+
+	public void delete(Cliente obj) {
+		find(obj.getId());
+		try {
+			repo.deleteById(obj.getId());
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque existem entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+
+	
 	
 }
