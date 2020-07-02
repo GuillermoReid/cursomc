d9bd0e4e433dedcb7756f93f0ca63de2576b189f
Guillermo Reid <g.guireid@gmail.com>
2020-07-02 10:56:08 -0300

    Validacao sintatica com BeanValidation

--- src/main/java/com/reidnet/cursomc/services/CategoriaService.java
+++ src/main/java/com/reidnet/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.reidnet.cursomc.domain.Categoria;
+import com.reidnet.cursomc.dto.CategoriaDTO;
 import com.reidnet.cursomc.repositories.CategoriaRepository;
 import com.reidnet.cursomc.services.exceptions.DataIntegrityException;
 import com.reidnet.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,11 +54,17 @@ public class CategoriaService {
 		return repo.findAll();
 
 	}
-	
+
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
+		
+	}
+	
 
 }
