3afc0edf8e987a062a0e6db2b6b84cd44c4622a5
Guillermo Reid <g.guireid@gmail.com>
2020-08-07 13:11:44 -0300

    Implementando toString de Pedido

--- src/main/java/com/reidnet/cursomc/services/PedidoService.java
+++ src/main/java/com/reidnet/cursomc/services/PedidoService.java
@@ -14,58 +14,62 @@ import com.reidnet.cursomc.domain.enums.EstadoPagamento;
 import com.reidnet.cursomc.repositories.ItemPedidoRepository;
 import com.reidnet.cursomc.repositories.PagamentoRepository;
 import com.reidnet.cursomc.repositories.PedidoRepository;
-import com.reidnet.cursomc.repositories.ProdutoRepository;
 import com.reidnet.cursomc.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private PedidoRepository repo;
 
 	@Autowired
+	private ClienteService clienteService;
+
+	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
-	private PagamentoRepository  pagamentoRepository;
-	
+	private PagamentoRepository pagamentoRepository;
+
 	@Autowired
 	private BoletoService boletoService;
-	
-	public Pedido find(Integer id){
-		
+
+	public Pedido find(Integer id) {
+
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-	
+
 	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
 
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		
-		for(ItemPedido ip : obj.getItens()) {
+
+		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
-		
+
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
+
 		return obj;
-	} 
-	
-	
+	}
+
 }
