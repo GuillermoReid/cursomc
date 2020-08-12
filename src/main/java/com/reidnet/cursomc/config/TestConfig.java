81d043b01cb9aff0326afacf7f9642ed077d1d37
Guillermo Reid <g.guireid@gmail.com>
2020-08-12 17:09:58 -0300

    Implementando autorizacao

--- src/main/java/com/reidnet/cursomc/config/TestConfig.java
+++ src/main/java/com/reidnet/cursomc/config/TestConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Profile;
 import com.reidnet.cursomc.services.DbService;
 import com.reidnet.cursomc.services.EmailService;
 import com.reidnet.cursomc.services.MockMailService;
-import com.reidnet.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("test")
